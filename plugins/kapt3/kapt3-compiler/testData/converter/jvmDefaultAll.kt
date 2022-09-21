649aeb48b204aff7f67b42a50921337f86040d22
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-09-21 10:40:15 +0300

    ~~ mute some tests because of problems in LC ~~

--- plugins/kapt3/kapt3-compiler/testData/converter/jvmDefaultAll.kt
+++ plugins/kapt3/kapt3-compiler/testData/converter/jvmDefaultAll.kt
@@ -1,3 +1,4 @@
+// FIR_BLOCKED: LC don't support DefaultImpls
 // !JVM_DEFAULT_MODE: all
 
 interface Foo {
--- plugins/kapt4/kapt4-compiler/test/org/jetbrains/kotlin/kapt4/TemporaryKapt4Suppressor.kt
+++ plugins/kapt4/kapt4-compiler/test/org/jetbrains/kotlin/kapt4/TemporaryKapt4Suppressor.kt
@@ -6,8 +6,11 @@
 package org.jetbrains.kotlin.kapt4
 
 import org.jetbrains.kotlin.kapt4.Kapt4Directives.FIR_ALMOST_DONE
+import org.jetbrains.kotlin.kapt4.Kapt4Directives.FIR_BLOCKED
 import org.jetbrains.kotlin.test.WrappedException
+import org.jetbrains.kotlin.test.directives.model.Directive
 import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
+import org.jetbrains.kotlin.test.directives.model.SimpleDirective
 import org.jetbrains.kotlin.test.directives.model.SimpleDirectivesContainer
 import org.jetbrains.kotlin.test.model.AfterAnalysisChecker
 import org.jetbrains.kotlin.test.services.TestServices
@@ -19,16 +22,22 @@ class TemporaryKapt4Suppressor(testServices: TestServices) : AfterAnalysisChecke
         get() = listOf(Kapt4Directives)
 
     override fun suppressIfNeeded(failedAssertions: List<WrappedException>): List<WrappedException> {
-        if (!testServices.moduleStructure.modules.any { FIR_ALMOST_DONE in it.directives }) {
-            return failedAssertions
+        val hasFailures = failedAssertions.isNotEmpty()
+        val suppressionDirectives = listOf(FIR_ALMOST_DONE, FIR_BLOCKED)
+        if (suppressionDirectives.any { suppressedByDirective(it, hasFailures) }) return emptyList()
+        return failedAssertions
+    }
+
+    private fun suppressedByDirective(directive: Directive, hasFailures: Boolean): Boolean {
+        val hasDirective = testServices.moduleStructure.modules.any { directive in it.directives }
+        if (hasDirective && !hasFailures) {
+            testServices.assertions.fail { "Test passes, remove $directive directive" }
         }
-        if (failedAssertions.isEmpty()) {
-            testServices.assertions.fail { "Test passes, remove $FIR_ALMOST_DONE directive" }
-        }
-        return emptyList()
+        return hasDirective
     }
 }
 
 object Kapt4Directives : SimpleDirectivesContainer() {
     val FIR_ALMOST_DONE by directive("This tests is almost passes with KAPT4")
+    val FIR_BLOCKED by stringDirective("Blocked by light classes")
 }
