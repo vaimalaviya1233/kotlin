490d5dd6b9004810f988c2ce1821a6ac74181816
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2022-12-05 18:02:22 +0100

    [FIR] Create separated FirBlock for loop local val

--- compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/converter/ExpressionsConverter.kt
+++ compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/converter/ExpressionsConverter.kt
@@ -1118,10 +1118,8 @@ class ExpressionsConverter(
                 // So, prepare the loop target after building the condition.
                 target = prepareTarget(forLoop)
             }.configure(target) {
-                // NB: just body.toFirBlock() isn't acceptable here because we need to add some statements
                 buildBlock block@{
                     source = blockNode?.toFirSourceElement()
-                    statements += convertLoopBody(blockNode).statements
                     val valueParameter = parameter ?: return@block
                     val multiDeclaration = valueParameter.destructuringDeclaration
                     val firLoopParameter = generateTemporaryVariable(
@@ -1145,10 +1143,11 @@ class ExpressionsConverter(
                             firLoopParameter,
                             tmpVariable = true
                         )
-                        statements.addAll(0, destructuringBlock.statements)
+                        statements.addAll(destructuringBlock.statements)
                     } else {
-                        statements.add(0, firLoopParameter)
+                        statements.add(firLoopParameter)
                     }
+                    statements += convertLoopBody(blockNode)
                 }
             }
         }
