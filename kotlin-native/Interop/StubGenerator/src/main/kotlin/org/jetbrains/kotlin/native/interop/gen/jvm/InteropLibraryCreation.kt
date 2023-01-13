25d50b773de83575902bf80e63e5c7707ba7b11b
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-01-13 11:46:47 +0100

    Make KotlinLibraryVersioning more type-safe

--- kotlin-native/Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/jvm/InteropLibraryCreation.kt
+++ kotlin-native/Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/jvm/InteropLibraryCreation.kt
@@ -40,7 +40,7 @@ fun createInteropLibrary(
             libraryVersion = null,
             abiVersion = KotlinAbiVersion.CURRENT,
             compilerVersion = CompilerVersion.CURRENT.toString(),
-            metadataVersion = KlibMetadataVersion.INSTANCE.toString(),
+            metadataVersion = KlibMetadataVersion.INSTANCE,
     )
     val libFile = File(outputPath)
     val unzippedDir = if (nopack) libFile else org.jetbrains.kotlin.konan.file.createTempDir("klib")
