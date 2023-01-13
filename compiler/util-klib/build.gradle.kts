3d5600f9e7021fa651294e9d9788c3af19551e2c
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-01-13 13:59:38 +0100

    implementation -> compileOnly

--- compiler/util-klib/build.gradle.kts
+++ compiler/util-klib/build.gradle.kts
@@ -8,7 +8,7 @@ description = "Common klib reader and writer"
 dependencies {
     api(kotlinStdlib())
     api(project(":kotlin-util-io"))
-    implementation(project(":core:metadata"))
+    compileOnly(project(":core:metadata"))
     testImplementation(commonDependency("junit:junit"))
 }
 
