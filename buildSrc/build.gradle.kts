673ee4f4c67437c87b3e7674a46f6094d736267b
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-01-16 16:43:47 +0100

    Hacky hack

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -113,6 +113,7 @@ sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourc
         kotlin.srcDir("../kotlin-native/build-tools/src/main/kotlin")
         kotlin.srcDir("../kotlin-native/tools/kotlin-native-gradle-plugin/src/main/kotlin")
         kotlin.srcDir("../compiler/util-klib/src")
+        kotlin.include("../core/metadata/src/org/jetbrains/kotlin/metadata/deserialization/BinaryVersion.kt")
         kotlin.srcDir("../native/utils/src")
     }
     kotlin.srcDir(project.kotlinNativeVersionSrc())
