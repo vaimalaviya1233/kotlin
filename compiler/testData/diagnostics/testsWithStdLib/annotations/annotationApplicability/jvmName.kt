453fbc69f3f752b3027af8f7d7c45f6347c4be08
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-03-15 15:47:03 +0100

    Add test

--- compiler/testData/diagnostics/testsWithStdLib/annotations/annotationApplicability/jvmName.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/annotationApplicability/jvmName.kt
@@ -89,7 +89,13 @@ class D: AB() {
 }
 
 interface Intf {
-    <!INAPPLICABLE_JVM_NAME!>@get:JvmName("getBar")<!> // no error in IDE
-    <!INAPPLICABLE_JVM_NAME!>@set:JvmName("setBar")<!> // no error in IDE
+    <!INAPPLICABLE_JVM_NAME!>@get:JvmName("getBar")<!>
+    <!INAPPLICABLE_JVM_NAME!>@set:JvmName("setBar")<!>
     var foo: Int
+
+    val getter: Int
+        @get:JvmName("Intf_getter") get
+
+    var setter: Int
+        <!INAPPLICABLE_TARGET_ON_PROPERTY!>@set:JvmName("Intf_getter")<!> set // KT-15470
 }
