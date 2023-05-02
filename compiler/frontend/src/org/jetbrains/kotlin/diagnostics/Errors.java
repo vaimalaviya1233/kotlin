7ce83e36ed8c247788268902d953aec9aefe4ee4
Anastasiia Spaseeva <anastasiia.spaseeva@jetbrains.com>
2023-05-02 15:53:25 +0200

    test commit

--- compiler/frontend/src/org/jetbrains/kotlin/diagnostics/Errors.java
+++ compiler/frontend/src/org/jetbrains/kotlin/diagnostics/Errors.java
@@ -2,9 +2,8 @@
  * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
-
+TEST
 package org.jetbrains.kotlin.diagnostics;
-
 import com.google.common.collect.ImmutableSet;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
