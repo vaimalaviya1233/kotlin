23b8789606b6623f47d56ed116b848fc6959ac5e
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2023-06-13 18:33:46 +0200

    KT-59287 [SLC] Keep private methods in interfaces

--- compiler/testData/asJava/lightClasses/lightClassByFqName/compilationErrors/PrivateInTrait.fir.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/compilationErrors/PrivateInTrait.fir.java
@@ -1,9 +1,9 @@
 public abstract interface PrivateInTrait /* PrivateInTrait*/ {
-  private abstract java.lang.String getN();//  getN()
+  private java.lang.String getN();//  getN()
 
-  private abstract java.lang.String getNn();//  getNn()
+  private java.lang.String getNn();//  getNn()
 
-  private abstract void setNn(java.lang.String);//  setNn(java.lang.String)
+  private void setNn(java.lang.String);//  setNn(java.lang.String)
 
   public static final class DefaultImpls /* PrivateInTrait.DefaultImpls*/ {
     private final java.lang.String getN();//  getN()
