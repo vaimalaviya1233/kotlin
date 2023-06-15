c58f907b72779dda21a9f09f1efe939f64af5fcb
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2023-06-15 13:09:14 +0200

    Bumped max supported gradle version to 8.0

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -27,7 +27,7 @@ interface TestVersions {
         const val G_8_1 = "8.1"
         const val MIN_SUPPORTED = minSupportedGradleVersion
         const val MIN_SUPPORTED_KPM = G_7_0
-        const val MAX_SUPPORTED = G_7_6
+        const val MAX_SUPPORTED = G_8_0
     }
 
     object Kotlin {
